5a721d146d623ebf1bc9eff355f0419c28ae597c
alamine95 <lsow313@gmail.com>
2022-02-21 02:30:23 +0000

    filter

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,14 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-    const products = await Product.find({}).sort('name')
+    const products = await Product.find({ price: { $gt: 30 }})
+    .sort('name')
+    .select('name price')
     res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
-    const {featured, company, name} = req.query
+    const {featured, company, name, sort, fields, numericFilters} = req.query
     const queryObject = {}
     if (featured) { 
         queryObject.featured = featured === 'true' ? true : false
@@ -17,8 +19,36 @@ const getAllProducts = async (req, res) => {
     if (name) {
         queryObject.name = {$regex: name, $options: 'i'}
     }
-    console.log(queryObject)
-    const products = await Product.find(queryObject)
+    // console.log(queryObject)
+    let result = Product.find(queryObject)
+    if(sort){
+        const sortList = sort.split(',').join(' ');
+        result = result.sort(sortList)
+    } else {
+        result = result.sort('createdAt')
+    }
+    if (fields) {
+        const fieldsList = fields.split(',').join(' ');
+        result = result.select(fieldsList)
+    }
+    if (numericFilters) {
+        const operatorMap = {
+            '>': '$gt',
+            '>=': '$gte',
+            '=': '$eq',
+            '<': '$lt',
+            '<=': '$lte',
+        }
+        const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+        let filters = numericFilters.replace(regEx, (match) => `-${operatorMap[match]}-`);
+        console.log(numericFilters)
+    }
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page -1) * limit;
+
+    result = result.skip(skip).limit(limit)
+    const products = await result
     res.status(200).json({ products, nbHits: products.length })
 }
 
