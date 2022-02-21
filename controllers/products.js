d9836ce05e5c32f4541f726f4ba93e5544360dad
alamine95 <lsow313@gmail.com>
2022-02-21 21:02:56 +0000

    filter

--- controllers/products.js
+++ controllers/products.js
@@ -41,8 +41,15 @@ const getAllProducts = async (req, res) => {
         }
         const regEx = /\b(<|>|>=|=|<|<=)\b/g;
         let filters = numericFilters.replace(regEx, (match) => `-${operatorMap[match]}-`);
-        console.log(numericFilters)
+        const options = ['price','rating'];
+        filters = filters.split(',').forEach((item) => {
+            const [field, operator, value] = item.split('-');
+            if (options.includes(field)) {
+              queryObject[field] = { [operator]: Number(value) };
+            }
+          });
     }
+    console.log(queryObject)
     const page = Number(req.query.page) || 1
     const limit = Number(req.query.limit) || 10
     const skip = (page -1) * limit;
